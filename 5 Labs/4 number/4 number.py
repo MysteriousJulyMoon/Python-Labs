f3a3c09645e8238290e55ed61d64505edc6b977e
Julia <150614653+MysteriousJulyMoon@users.noreply.github.com>
2024-04-22 10:55:39 +0200

    Update 4 number.py

--- 5 Labs/4 number/4 number.py
+++ 5 Labs/4 number/4 number.py
@@ -1,35 +1,3 @@
-'''
-import json
-import csv
-import os
-import sys
-
-def json_to_csv(json_filename):
-    with open(json_filename, 'r') as json_file:
-        data = json.load(json_file)
-
-    csv_filename = os.path.splitext(json_filename)[0] + ".csv"
-    root_name = list(data.keys())[0]
-
-    with open(csv_filename, 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(data[root_name][0].keys())
-
-        for item in data[root_name]:
-            writer.writerow(item.values())
-
-    return csv_filename
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python json2csv.py example.json")
-    else:
-        json_filename = sys.argv[1]
-        csv_filename = json_to_csv(json_filename)
-        print(f"CSV файл создан: {csv_filename}")
-'''
-
-
 import json
 import csv
 import sys
